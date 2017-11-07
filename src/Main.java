5c5e2a0262caf0c212a7444115f96fe2f5167d4c
Ivan Murzak <ivan.d.murzak@gmail.com>
2017-11-07 20:39:54 +0200

    Создал первый класс

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // Привет всем! :)
+        // Это коментарий
+
+        // следующая строка выведет в консоль Hello World!
+        System.out.println("Hello World!");
+        System.out.println();
+
+        System.out.println("2 + 2 = " + (2 + 2));
+
+        System.out.println("Строка 1\n\tСтрока 2\n\t\tСтрока 3");
+    }
+}

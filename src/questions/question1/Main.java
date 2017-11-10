e119a55ad7d771c8f05ac53484bac110e879e998
Ivan Murzak <ivan.d.murzak@gmail.com>
2017-11-10 22:20:51 +0200

    Примеры к уроку

--- src/questions/question1/Main.java
+++ src/questions/question1/Main.java
@@ -3,5 +3,6 @@ package questions.question1;
 public class Main {
     public static void main(String[] args) {
         System.out.println(MainQuestion.dollar(10, 6));
+        System.out.println(MainQuestion.dollar(10, 100));
     }
 }

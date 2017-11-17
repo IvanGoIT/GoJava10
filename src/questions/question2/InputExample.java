0f923bd304f6db7b09d4487bfb214fd614ec9045
Ivan Murzak <ivan.d.murzak@gmail.com>
2017-11-17 20:41:25 +0200

    Пример ввода / вывода

---
+++ src/questions/question2/InputExample.java
@@ -0,0 +1,17 @@
+package questions.question2;
+
+import java.util.Scanner;
+
+public class InputExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите пожалуйста ваше имя:");
+        String name = scanner.nextLine();
+        System.out.println("Введите пожалуйста ваше любимое число:");
+        int favoriteNumber = scanner.nextInt();
+
+        System.out.println("Спасибо " + name);
+        System.out.println("Ваше любимое число - " + favoriteNumber);
+    }
+}

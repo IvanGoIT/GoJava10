7f44c3b08a14e8f61772fd095dd3cebf4e8d8db8
Ivan Murzak <ivan.d.murzak@gmail.com>
2017-12-08 21:34:34 +0200

    Ответ на вопрос

--- src/questions/question5/CodeExample.java
+++ src/questions/question5/CodeExample.java
@@ -7,12 +7,19 @@ public class CodeExample {
     public static void main(String[] args) {
         showOld();
         show();
+
+        openDoor();
+        show();
+
+        toggleDoor();
+        show();
     }
 
     public static void show() {
         System.out.println("*** CarDoor state ***");
         System.out.println("Car door - " + (doorState ? "open" : "close"));
         System.out.println("Car window - " + (windowState ? "open" : "close"));
+        System.out.println();
     }
 
     public static void showOld() {
@@ -23,5 +30,17 @@ public class CodeExample {
         if (!windowState) {
             System.out.println("Car window - close");
         } else System.out.println("Car window - open");
+    }
+
+    public static void openDoor() {
+        if (!doorState) toggleDoor();
+    }
+
+    public static void closeDoor() {
+        if (doorState) toggleDoor();
+    }
+
+    public static void toggleDoor() {
+        doorState = !doorState; //поменяли значение на противоположное
     }
 }

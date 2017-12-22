654a01a5ed131d87af0845b1d65e48fb4e04414e
Ivan Murzak <ivan.d.murzak@gmail.com>
2017-12-22 20:52:48 +0200

    Добавил пример с проигрыванием аудио

---
+++ src/questions/question5_2/Main.java
@@ -0,0 +1,41 @@
+package questions.question5_2;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage)
+    {
+        //Add a scene
+        Group root = new Group();
+        Scene scene = new Scene(root, 500, 200);
+
+        String path = "files/question5_2/put.mp3";
+        Media pick = new Media(new File(path).toURI().toString());
+        MediaPlayer player = new MediaPlayer(pick);
+        player.play();
+
+        //Add a mediaView, to display the media. Its necessary !
+        //This mediaView is added to a Pane
+        MediaView mediaView = new MediaView(player);
+        ((Group)scene.getRoot()).getChildren().add(mediaView);
+
+        //show the stage
+        primaryStage.setTitle("Media Player");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}

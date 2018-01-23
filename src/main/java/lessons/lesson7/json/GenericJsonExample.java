84d464cc2bc58b403fd3ee083cb22b69872d81a5
Ivan Murzak <ivan.d.murzak@gmail.com>
2018-01-23 21:26:38 +0200

    Добавил парсинг json с дженерик типами

--- src/main/java/lessons/lesson7/json/GenericJsonExample.java
+++ src/main/java/lessons/lesson7/json/GenericJsonExample.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson.TypeReference;
 import lessons.lesson7.json.entity.DogEntity;
 import lessons.lesson7.json.entity.Node;
 import lessons.lesson7.json.entity.User;
+import questions.question5.entities.Dog;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +24,7 @@ public class GenericJsonExample {
         String json = JSON.toJSONString(nodes);
         System.out.println(json);
 
+        System.out.println();
 
         ArrayList<Node<DogEntity>> nodes2 = new ArrayList<>();
         nodes2.add(new Node<>(new DogEntity("bobik", 5), "the best dog"));
@@ -31,7 +33,18 @@ public class GenericJsonExample {
         String json2 = JSON.toJSONString(nodes2);
         System.out.println(json2);
 
-        // ArrayList<Node<User>> nodesClone = JSON.parseArray(json, Node<User>.getClass());
 
+        System.out.println();
+        System.out.println();
+
+        TypeReference<ArrayList<Node<User>>> typeRef = new TypeReference<ArrayList<Node<User>>>() {};
+        ArrayList<Node<User>> nodesClone = JSON.parseObject(json, typeRef);
+        System.out.println(nodesClone);
+
+        System.out.println();
+
+        TypeReference<ArrayList<Node<DogEntity>>> typeRef2 = new TypeReference<ArrayList<Node<DogEntity>>>() {};
+        ArrayList<Node<DogEntity>> nodesClone2 = JSON.parseObject(json, typeRef2);
+        System.out.println(nodesClone2);
     }
 }
